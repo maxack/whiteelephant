97cb8213ce271a07901b0b873640b3ec0f413ed9
Max Ackerman <max@Maxs-iMac.local>
2015-12-17 14:30:02 -0700

    add images

--- app/main-view-model.js
+++ app/main-view-model.js
@@ -11,14 +11,36 @@ function imageFromSource(imageName) {
     return imageSourceModule.fromFile(fileSystemModule.path.join(__dirname, directory + imageName));
 }
 
-var item1 = {itemImage: imageFromSource("01.jpg"), itemName: "max"};
-var item2 = {itemImage: imageFromSource("02.jpg"), itemName: "max"};
-var item3 = {itemImage: imageFromSource("03.jpg"), itemName: "max"};
-var item4 = {itemImage: imageFromSource("04.jpg"), itemName: "max"};
-var item5 = {itemImage: imageFromSource("05.jpg"), itemName: "max"};
-var item6 = {itemImage: imageFromSource("06.jpg"), itemName: "max"};
-var item7 = {itemImage: imageFromSource("07.jpg"), itemName: "max"};
-var item8 = {itemImage: imageFromSource("08.jpg"), itemName: "max"};
+/**
+ * Randomize array element order in-place.
+ * Using Durstenfeld shuffle algorithm.
+ */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+var item1 = {itemImage: imageFromSource("01.jpg"), itemName: "Matt"};
+var item2 = {itemImage: imageFromSource("02.jpg"), itemName: "Seth"};
+var item3 = {itemImage: imageFromSource("03.jpg"), itemName: "Nicole"};
+var item4 = {itemImage: imageFromSource("04.jpg"), itemName: "Samantha"};
+var item5 = {itemImage: imageFromSource("05.jpg"), itemName: "Brian"};
+var item6 = {itemImage: imageFromSource("06.jpg"), itemName: "Jon"};
+var item7 = {itemImage: imageFromSource("07.jpg"), itemName: "Chris S"};
+var item8 = {itemImage: imageFromSource("08.jpg"), itemName: "Jake"};
+var item9 = {itemImage: imageFromSource("09.jpg"), itemName: "Jarom"};
+var item10 = {itemImage: imageFromSource("10.jpg"), itemName: "Chris H"};
+var item11 = {itemImage: imageFromSource("11.jpg"), itemName: "Alain"};
+var item12 = {itemImage: imageFromSource("12.jpg"), itemName: "max"};
+
+var people = [item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12];
+
+shuffleArray(people);
 
 //localImagesArray.push([item1, item2, item3,item6, item7, item8]);
 
@@ -33,8 +55,9 @@ Object.defineProperty(elephantsModel, "photoItems", {
 });
 
 elephantsModel.tapAction = function() {
-    localImagesArray.push(item4);
-    localImagesArray.push(item5);
+    if (people.length > 0) {
+        localImagesArray.push(people.pop());
+    } 
 };
 
 exports.elephantsModel = elephantsModel;
